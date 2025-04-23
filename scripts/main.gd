52fc4da2fc1d73a99cd8e76bda6e189aa5fce313
Adolfo Gante <adolfogante@gmail.com>
2025-04-23 14:24:27 -0700

    better platform generation

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,14 +1,37 @@
 extends Node3D
 @onready var main = $"."
 
+func spawnPlatform(platform_position: Vector3) -> Vector3:
+	var platform_scene = preload("res://scenes/platform.tscn")
+	var platform = platform_scene.instantiate()
+	platform.position = platform_position
+	print("Platform: " + str(platform.position))
+	add_child(platform)
+	return platform.position	
+	
+func calcNewPosition(prev_position: Vector3):
+	var next_x = prev_position.x + randi_range(-5, 5)
+	var next_y = prev_position.y + 1
+	var next_z = prev_position.z + randi_range(-5, 5)
+	
+	var next_position = Vector3(
+		next_x,
+		next_y,
+		next_z
+	)
+	
+	return next_position
+
+func spawnPlatforms(amount: int):
+	var next_position = Vector3(5, 0, 5)
+	var prev_position
+	
+	for i in range(amount):
+		# Vector3(randi() % 7, (i), randi() % 8)
+		prev_position = spawnPlatform(next_position)
+		next_position = calcNewPosition(prev_position)
+	pass
+	
 func _ready():
-	var heart_scene = preload("res://scenes/platform.tscn")
-	
-	var hearts = []
-	
-	for i in range(50):
-		var heart = heart_scene.instantiate()
-		heart.position = Vector3(randi() % 9, randi() % 9, randi() % 9)
-		print("Platform: " + str(i) + " " + str(heart.position))
-		hearts.append(heart)
-		add_child(hearts[i])
+	spawnPlatforms(50)
+	pass
